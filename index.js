fdd19227e364cb9affa1ab2012b4a1e50db3c672
Nikapwente <hrsmajor@gmail.com>
2023-10-06 10:38:37 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    let catsCopy = [...cats, name];
+    return catsCopy;
+}
+
+function prependCat(name) {
+    let catsCopy = [name, ...cats];
+    return catsCopy;
+}
+
+function removeLastCat() {
+    let catsCopy = cats.slice(0,-1);
+    return catsCopy;
+}
+
+function removeFirstCat() {
+    let catsCopy = cats.slice(1,cats.length);
+    return catsCopy;
+}
